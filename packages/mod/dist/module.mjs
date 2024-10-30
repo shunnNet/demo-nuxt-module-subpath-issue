f5f7048419daddb96fc0d4fe48eef4b1dceb8c39
shunnNet <wendell20904102@gmail.com>
2024-10-31 00:47:32 +0800

    update

--- packages/mod/dist/module.mjs
+++ packages/mod/dist/module.mjs
@@ -1,12 +1,16 @@
-import jiti from "file:///Users/net/repository/mod/packages/module/node_modules/.pnpm/jiti@1.21.6/node_modules/jiti/lib/index.js";
+import { defineNuxtModule, createResolver, addPlugin } from '@nuxt/kit';
 
-/** @type {import("/Users/net/repository/mod/packages/module/src/module")} */
-const _module = jiti(null, {
-  "esmResolve": true,
-  "interopDefault": true,
-  "alias": {
-    "my-module": "/Users/net/repository/mod/packages/module"
+const module = defineNuxtModule({
+  meta: {
+    name: "my-module",
+    configKey: "myModule"
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  setup(_options, _nuxt) {
+    const resolver = createResolver(import.meta.url);
+    addPlugin(resolver.resolve("./runtime/plugin"));
   }
-})("/Users/net/repository/mod/packages/module/src/module.ts");
+});
 
-export default _module;
+export { module as default };
